5a7887f3d34edf24de12f4c9aa24f5638d895d38
bjorngronntun <bjorngronntun@gmail.com>
2018-02-03 01:13:11 +0100

    Added JSON

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from random import randint
+import json
 
 app = Flask(__name__)
 
+with open('data/data.json') as f:
+    data = json.load(f)
 @app.route('/')
 def home():
     tall1 = randint(1,12)
     tall2 = randint(1,12)
     return 'Hva er {} ganger {}?'.format(tall1, tall2)
 
+@app.route('/test')
+def test():
+    land = data['land']
+    return render_template('test.html', land=land)
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
